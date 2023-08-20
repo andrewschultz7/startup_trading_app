a3ccc274edf2c6070c8a20a3d8ebddcf96298e62
Andrew Schultz <andrew7primary@gmail.com>
2023-08-20 17:48:41 -0600

    added tos module

--- tos/tos.py
+++ tos/tos.py
@@ -1,3 +1,33 @@
 # open tos
 # input password
 # hit enter
+
+from dotenv import load_dotenv
+import os, time
+from pywinauto import Application
+import pyautogui
+
+password = os.environ.get('TOSPASSWORD')
+window_title = "Logon to thinkorswim"
+pwd_field = pyautogui.locateCenterOnScreen('screenshots\tos_password.PNG', confidence=0.8)
+
+os.system('start explorer "C:\\Program Files\\thinkorswim\\thinkorswim.exe"')
+
+exit_loop = False
+while not exit_loop:
+    time.sleep(1)
+    try:
+        tos_app = Application(backend='uia').connect(title=window_title)
+        window = tos_app.window(title=window_title)
+        exit_loop = True
+    except:
+        try:
+            tos_app = Application(backend='win32').connect(title=window_title)
+            window = tos_app.window(title=window_title)
+            exit_loop = True
+        except:
+            pass
+
+pyautogui.moveTo(pwd_field)
+pyautogui.typewrite(password)
+pyautogui.press('Enter')
